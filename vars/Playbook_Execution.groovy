a560a2f9296f077ce7f1c73f4ce045750caf1e9e
Priyanshu498 <priyanshu1912003@gmail.com>
2024-08-31 00:36:26 +0530

    Update Playbook_Execution.groovy

--- vars/Playbook_Execution.groovy
+++ vars/Playbook_Execution.groovy
@@ -1,6 +1,9 @@
 def call() {
-    stage('playbook execution') {
-        sh "ansible-playbook -i /Users/priyanshu/Apache-tom/assignmet_0n_tool/tomcat/tests/inventory /Users/priyanshu/Apache-tom/assignmet_0n_tool/tomcat/tests/test.yml"
+    stage('Playbook Execution') {
+        // Set the PATH variable to include the location of ansible-playbook
+        withEnv(["PATH+AN=/opt/homebrew/bin"]) {
+            sh "ansible-playbook -i /Users/priyanshu/Apache-tom/assignmet_0n_tool/tomcat/tests/inventory /Users/priyanshu/Apache-tom/assignmet_0n_tool/tomcat/tests/test.yml"
+        }
     }
 }
 
